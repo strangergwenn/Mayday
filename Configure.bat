bc11148e8fe74cfe038e19a43c78861007437ace
Gwennaël Arbona <gwennael.arbona@gmail.com>
2022-01-02 23:56:31 +0100

    Update to VS 2022

--- Configure.bat
+++ Configure.bat
@@ -4,6 +4,6 @@ set build="build/Win64"
 if not exist "%build%" mkdir %build%
 pushd %build%
 
-cmake -G "Visual Studio 16 2019" -A "x64" "%~dp0" 
+cmake -G "Visual Studio 17 2022" -A "x64" "%~dp0" 
 
 popd
